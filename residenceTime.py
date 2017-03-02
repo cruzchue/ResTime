278a38d1157d54e8adb714ecbdf804cd502d8197
Eduardo R. Cruz-Chu <cruzchue@ethz.ch>
2017-03-02 17:55:18 +0100

    Update residenceTime.py

--- residenceTime.py
+++ residenceTime.py
@@ -64,12 +64,16 @@ def sumSignedInt ( OnOffArray ):
 ##########
 
 
-def flipCutOff ( OnOffSum, cutOff ):
+
+
+def flipCutOff ( OnOffSum, cutOff, ends=0 ):
     #
     #  INPUT
     # =======
     # - OnOffSum : 1D array of signed integers
     # - cutOff   : jump frames
+    # - ends     : take into account end signs (value 1) or not (value 0)
+    #             
     #
     #  OUTPUT
     # ========
@@ -78,15 +82,35 @@ def flipCutOff ( OnOffSum, cutOff ):
 
     # flip to negative sign
     cutOff=-1*abs(cutOff)
- 
+    
+    
+    # keep start and end values for OnOffSum 
+    if ends == 0 :
+        OnOff1st=OnOffSum[:,0]
+        OnOffLast=OnOffSum[:,-1]
+        OnOffMid=OnOffSum[:,1:-1]    
+    else:
+        OnOffMid=OnOffSum
+
+    
     # find values higher than cutOff
-    ind=((OnOffSum>=cutOff) & (OnOffSum<=0))
+    ind=((OnOffMid>=cutOff) & (OnOffMid<=0))
+    
     
     # flip sign by adding twice the cutoff values
-    OnOffCutOff=OnOffSum+( OnOffSum*(ind*-2))
+    OnOffCutOff=OnOffMid+( OnOffMid*(ind*-2))
+
+    
+    # return start and end values to OnOffSum 
+    if ends == 0 :
+        OnOffCutOff=np.append(OnOff1st,OnOffCutOff)
+        OnOffCutOff=np.append(OnOffCutOff,OnOffLast)
+        OnOffCutOff=OnOffCutOff.reshape((1, len(OnOffCutOff) ))
+
 
     # out
     return OnOffCutOff
+
 
 
 
@@ -199,6 +223,173 @@ def rtInVol ( mdUniverse, volume, selection, jumpBack ):
     return OnOffPos
 
 
+
+# CHECK
+def bool2frame( inOutVol, jumpBack ):
+    #
+    #  INPUT
+    # =======
+    # - inOutVol  : 1D array of in-out boolean values
+    # - jumpBack : blind frames 
+    #
+    #  OUTPUT
+    # ========
+    # - outFrame : 2D array with start and end frames
+    #
+
+    # add values
+    OnOffSum=sumSignedInt(inOutVol)
+
+    # flip signs using jumBack
+    OnOffCutOff=flipCutOff( OnOffSum, jumpBack )
+
+    # add values after sign flip
+    OnOffFrame=sumSignedInt(OnOffCutOff)
+
+    # start and end indexes
+    rangeFrame=np.cumsum(np.absolute(OnOffFrame))
+    startFrame=np.insert(rangeFrame,0,0)
+    startFrame=startFrame[0:-1]
+
+    endFrame=rangeFrame-1
+        
+    # reorganize output values
+    outFrame=np.zeros((0,2))
+    
+    iCount=0
+    for oneVal in OnOffFrame[0] :        
+        if oneVal > 0 :            
+            oneRange=np.zeros((1,2))
+            oneRange[0,0]=startFrame[iCount]
+            oneRange[0,1]=endFrame[iCount]
+            outFrame=np.vstack((outFrame,oneRange))            
+        iCount+=1
+            
+    # out
+    return outFrame
+
+
+
+def frameInVol ( mdUniverse, volume, selection, jumpBack ):
+    #
+    # frame ranges in volume,
+    # useful for single trajectory
+    #
+    #  INPUT
+    # =======
+    # - mdUniverse : MDanalysis universe
+    # - volume     : list of volumes
+    # - selection  : list of selections
+    # - jumpBack   : blind frames 
+    #
+    #
+    #  OUTPUT
+    # ========
+    # - OnOffPos : 1D array with fileter residence times
+    #
+
+    OnOffArray=inOutVol( mdUniverse, volume, selection )
+    OnOffFrame=bool2frame( OnOffArray, jumpBack )
+
+    return OnOffFrame
+
+##########
+
+
+def volFrame2bool ( inIndexFrameRange ):
+    #
+    # - inIndexFrameRange : [n,3] array
+    #    1st col atom number
+    #    2nd col start frame
+    #    3rd col end frame
+    #
+    # - outT : tuple of 2 arrays
+    #    1st array : atom or sel index
+    #    2nd array  : OnOff array
+    #
+    
+    # separate array
+    inM=inIndexFrameRange[:,0]
+    inM=inM.astype(int)
+    frameM=inIndexFrameRange[:,1:]
+    frameM=frameM.astype(int)
+    
+    
+    # sort indexes and frame
+    uniqueIndex=np.unique(inM)
+    maxFrame=np.amax(frameM)
+
+    
+    # fill On-Off values
+    outOnOff=np.zeros((0,maxFrame+1))
+
+    for eachIndex in uniqueIndex :
+        currOnOff=np.ones((1,maxFrame+1))
+        currOnOff=currOnOff*(-1)
+        
+        frameRange=frameM[np.where(inM==eachIndex)]
+        
+        for eachRange in frameRange :
+            startFrame=eachRange[0]
+            endFrame=eachRange[1]
+            currOnOff[0,startFrame:endFrame+1]=1
+        
+        outOnOff=np.vstack((outOnOff,currOnOff))
+
+    
+    # output
+    uniqueIndex=uniqueIndex.reshape((1, len(uniqueIndex) ))
+    outT=(uniqueIndex, outOnOff)
+    return outT
+
+
+##########
+
+def volFrameJumBack ( inIndexFrameRange, jumpBack ):
+
+    # transform volFrame data into OnOffArray
+    allIndexOnOff=volFrame2bool( inIndexFrameRange )
+
+    # separate input data
+    allIndex=allIndexOnOff[0]
+    allOnOff=allIndexOnOff[1]
+
+    # output matrix
+    outM=np.zeros((0,3))
+
+    # resort data
+    [iOnOff,jOnOff]=shape(allOnOff)
+    
+    iCount=0
+    while iCount < iOnOff :
+
+        # work with OnOff
+        currIndex=allIndex[0,iCount]        
+        currOnOff=allOnOff[iCount,:]
+        currOnOff=currOnOff.reshape((1, len(currOnOff) ))
+
+        # jump back
+        currFrameRange=bool2frame( currOnOff, jumpBack )
+
+        # index column
+        [iCurr,jCurr]=np.shape(currFrameRange)
+        colIndex=np.ones((iCurr,1))*currIndex
+
+        # join index with frame range
+        currIndexFrameRange=np.hstack((colIndex,currFrameRange ))        
+
+        # add to output matrix
+        outM=np.vstack((outM,currIndexFrameRange))
+
+        iCount+=1
+
+    return outM
+
+
+##################
+##################
+
+
 ##########
 
 # PENDING
@@ -210,8 +401,15 @@ def rtInVol ( mdUniverse, volume, selection, jumpBack ):
 #
 #
 #
+# NOTE: 2D arrays preferred over 1D
+#
 
 
+##################
+
+
+
+#################
 
 
 
